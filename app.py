fe3d8b352bed5e61fb754c1ff4e06e715197d645
Dan King <dan.king@vptech.io>
2026-02-20 07:25:19 -0800

    set up base framework

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Hello World from viewpoint.pro</h1>'
+    return '<h1>Greetings from San Diego!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
